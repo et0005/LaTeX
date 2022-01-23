8fdb7beb9497daa860605aa9744c0a422aad4d91
Elena <16710720+et0005@users.noreply.github.com>
2022-01-23 15:37:09 -0600

    Update file structure for much simpler use

---
+++ Lab Report/Course123/lab_02/code/lab2_1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+using namespace std;
+
+// -------------------------------------------------------------------
+//
+//    Lab:         ## - Name
+//    Due Date:    January 01st 1970
+//
+//    Author:      First Lastname
+//
+//    Description: Write a program that demonstrates these concepts:
+//
+//        1. One
+//        2. Two
+//
+// -------------------------------------------------------------------
+
+int main()
+{
+
+}
